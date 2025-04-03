e0dfaf838d3a2b9b5797903e7ba3e1b4a99a2225
Rogerio Istiraneopulos <roger88ist@gmail.com>
2025-04-02 23:18:25 -0400

    blueprint of a loop interface

--- interface.py
+++ interface.py
@@ -1,6 +1,35 @@
 from expense_tracker import ExpenseTracker
+import sys
 
-tracker = ExpenseTracker()
-tracker.add_expense()
-tracker.set_budget()
-tracker.calculate_expenses()
+class Interface:
+	def __init__(self):
+		self.tracker = ExpenseTracker()
+
+	def start(self):
+		proceed = True
+
+		while proceed:
+			ipt = self.ask_for_input()
+
+			if ipt == "5":
+				proceed = False
+
+	def ask_for_input(self):
+		self.display_menu()
+		return input("Enter Option:")
+
+
+	def display_menu(self):
+		self.clear_console()
+		print("1. Add Expense")
+		print("2. View Expenses")
+		print("3. Track Budget")
+		print("4. Save Expenses")
+		print("5. Exit")
+
+	def clear_console(self):
+	    sys.stdout.write("\033[H\033[J")  # ANSI escape code to clear screen
+	    sys.stdout.flush()
+
+
+Interface().start()
