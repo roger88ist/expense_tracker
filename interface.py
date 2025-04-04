764dc3776946d7310ffe08e66b881d0e01040c93
Rogerio Istiraneopulos <roger88ist@gmail.com>
2025-04-03 20:19:35 -0400

    switched to case statement

--- interface.py
+++ interface.py
@@ -11,16 +11,26 @@ class Interface:
 		while proceed:
 			ipt = self.ask_for_input()
 
-			if ipt == "5":
-				proceed = False
+			match ipt:
+				case "1":
+					self.tracker.add_expense()
+				case "2":
+					self.tracker.show_history()
+				case "3":
+					self.tracker.set_budget()
+					self.tracker.calculate_expenses()
+				case "4":
+					self.tracker.save_to_csv()
+				case "5":
+					proceed = False
 
 	def ask_for_input(self):
 		self.display_menu()
-		return input("Enter Option:")
+		return input("Enter Option: ")
 
 
 	def display_menu(self):
-		self.clear_console()
+		# self.clear_console()
 		print("1. Add Expense")
 		print("2. View Expenses")
 		print("3. Track Budget")
